d1113514067e3d660da212f1f6e66896f5d2a7e3
EphraimCompEng <madugba.finance@proton.me>
2026-02-25 07:28:52 +0000

    chore: ruff format pass

--- multiplied/tests/core/test_algorithm.py
+++ multiplied/tests/core/test_algorithm.py
@@ -4,11 +4,9 @@ import multiplied as mp
 
 @pytest.fixture
 def gen_resources_4b():
-    p = mp.Pattern(["a","a","b","b"])
+    p = mp.Pattern(["a", "a", "b", "b"])
     alg = mp.Algorithm(4)
     return p, alg
-
-
 
 
 @pytest.fixture
@@ -18,20 +16,19 @@ def gen_resources_8b():
     return p, alg
 
 
-
-def gen_resources(bits: int,*,a=0,b=0
-) -> tuple[mp.Matrix, mp.Pattern, mp.Algorithm]:
+def gen_resources(bits: int, *, a=0, b=0) -> tuple[mp.Matrix, mp.Pattern, mp.Algorithm]:
 
     m = mp.Matrix(bits, a=a, b=b)
     match bits:
         case 4:
-            p = mp.Pattern(["a","a","b","b"])
+            p = mp.Pattern(["a", "a", "b", "b"])
         case 8:
             p = mp.Pattern(["a", "a", "a", "b", "b", "b", "c", "c"])
         case _:
             raise ValueError(f"Unsupported number of bits: {bits}")
     alg = mp.Algorithm(bits)
     return m, p, alg
+
 
 def manual_test_exec_8(a: int, b: int) -> None:
     m, p, alg = gen_resources(8, a=a, b=b)
@@ -43,9 +40,7 @@ def manual_test_exec_8(a: int, b: int) -> None:
         print(i)
 
     print(int("".join(alg.matrix.matrix[0]), 2))
-    print(a*b)
-
-
+    print(a * b)
 
 
 def main():
