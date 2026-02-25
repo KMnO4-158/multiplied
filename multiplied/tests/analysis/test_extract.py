13f00be395d409626beb429a765e5f9bcb7667d3
EphraimCompEng <madugba.finance@proton.me>
2026-02-25 16:48:52 +0000

    tests: prep analysis test

---
+++ multiplied/tests/analysis/test_extract.py
@@ -0,0 +1,7 @@
+# from multiplied.analysis.extract import (
+#     pq_extract_stages,
+# )
+
+# create fixture, mocks? teardowns?
+
+def test_pq_extract_stages(): ...
---
+++ multiplied/tests/analysis/test_heatmap.py
@@ -0,0 +1,14 @@
+# from multiplied.analysis.heatmap import (
+#     df_global_heatmap,
+#     df_global_3d_heatmap,
+#     df_stage_heatmap,
+# )
+
+
+# create fixture, mocks? teardowns?
+
+def test_df_global_heatmap():...
+
+def test_df_global_3d_heatmap():...
+
+def test_df_stage_heatmap():...
