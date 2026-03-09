bcdb424b54f3437bc81ac1e589234492c181aed3
EphraimCompEng <madugba.finance@proton.me>
2026-03-09 00:18:26 +0000

    test: validated matrix_scatter

--- multiplied/testing/empty_template_harmony.py
+++ multiplied/testing/empty_template_harmony.py
@@ -26,8 +26,7 @@ def test_shallow_generator8() -> None:
     for m in mp.shallow_truth_table(truth8, alg8):
         mp.mprint(m)
 
-
-def main() -> None:
+def test_complex_templates() -> None:
     from multiplied.tests import REFERENCE
     alg = mp.Algorithm(8)
     empty_pattern = mp.Pattern(["a", "_", "_", "_", "_", "_", "_", "_"])
@@ -49,6 +48,35 @@ def main() -> None:
     print(alg)
     for i in alg.exec(15, 14).values():
         print(i)
+
+
+def main() -> None:
+    template = mp.Template(mp.Pattern(["a", "b", "b", "c", "c", "c", "_", "_"]))
+    print(template)
+    for k, v in mp.matrix_scatter(template.template, template.bounds).items():
+        print(k)
+        mp.mprint(v)
+
+
+    matrix = template.result
+    print(matrix)
+    for k, v in mp.matrix_scatter(matrix.matrix, template.bounds).items():
+        print(k)
+        mp.mprint(v)
+
+    matrix = mp.Matrix(8, a=12, b=2)
+    print(matrix)
+    for k, v in mp.matrix_scatter(matrix.matrix, template.bounds).items():
+        print(k)
+        mp.mprint(v)
+
+    template = mp.Template(mp.Pattern(["a", "b", "b", "c", "c", "c", "_", "_"]))
+    resultant = template.result
+    map = resultant.resolve_rmap()
+    print(map)
+    for k, v in mp.matrix_scatter(map.map, template.bounds).items():
+        print(k)
+        mp.mprint(v)
 
 if __name__ == "__main__":
     main()
