3c2482d32a0cf1b7968c8f421b0a0cbd8ea96bc5
EphraimCompEng <madugba.finance@proton.me>
2026-03-13 19:47:19 +0000

    chore: ruff format pass

--- multiplied/testing/parallel_truth_scope.py
+++ multiplied/testing/parallel_truth_scope.py
@@ -2,8 +2,6 @@ from time import perf_counter
 from typing import Generator
 import warnings
 import multiplied as mp
-# from pathlib import Path
-
 
 
 def _batch_truth_scope(
@@ -18,16 +16,15 @@ def _batch_truth_scope(
     slope = (max_in - min_in) / (max_out - min_out)
     print(slope)
 
-    if (max_in - min_in)**2 < 1000 or (max_out - min_out) < 256:
+    if (max_in - min_in) ** 2 < 1000 or (max_out - min_out) < 256:
         if workers > 1:
             warnings.warn("workers > 1 not recommended for small domains/ranges")
         yield (domain_, range_)
         return
 
-
     if workers == 2:
         adjusted_min_out = int((max_out - min_out) >> int(2 / (1 + slope)))  # heuristic
-        yield (domain_, (min_out, adjusted_min_out) )
+        yield (domain_, (min_out, adjusted_min_out))
         yield (domain_, (adjusted_min_out + 1, max_out))
         return
 
@@ -36,10 +33,8 @@ def _batch_truth_scope(
     print(f"in batch: {in_batch_size}")
     print(f"out batch: {out_batch_size}")
 
-
     first_last_ratio = in_batch_size / out_batch_size
     print(first_last_ratio)
-
 
     # heuristic balances operands / batch ===========================
     #
@@ -56,18 +51,17 @@ def _batch_truth_scope(
     acc = 0
     for n in range(min_out, max_out + 1):
         # over/under estimate is accounted for later
-        acc += (out_batch_size//n)
+        acc += out_batch_size // n
     print(acc)
 
     # -- distribute linear offsets ----------------------------------
     for i in range((workers)):
-        balance[i] = -int(((acc) * (1 - (slope))) // ((i + 12)))
+        balance[i] = -int(((acc) * (1 - (slope))) // (i + 12))
 
-    print(((max_in - min_in + 2) >> workers ))
+    print(((max_in - min_in + 2) >> workers))
     # -- distribute dyadic (?) offsets ------------------------------
-    for i in range((workers) >> ((max_in - min_in + 2) >> workers )):
+    for i in range((workers) >> ((max_in - min_in + 2) >> workers)):
         balance[-i - 1] += int(((acc) * (1 - slope))) >> (i + 3)
-
 
     print(sum(balance))
 
@@ -77,28 +71,27 @@ def _batch_truth_scope(
     # domain far from range
     if slope < 0.01:
         for i in range((workers)):
-            balance[-i - 1] += (rem >> int(log2(workers) + i))
+            balance[-i - 1] += rem >> int(log2(workers) + i)
 
         final = sum(balance)
         balance[-1] += -int(final * 0.95) + 1  # collect 95% of remainder to final batch
-        balance[(workers >> 1) -1] += -int(final * 0.05) # 5% applied to low midpoint
+        balance[(workers >> 1) - 1] += -int(final * 0.05)  # 5% applied to low midpoint
 
     # domain close to range
     else:
         for i in range((workers)):
-            balance[i] += (rem >> int(log2(workers)))
+            balance[i] += rem >> int(log2(workers))
 
     print("rem", rem)
     print(sum(balance))
     # ==============================================================
-
 
     print(balance)
 
     r_min_chunk = min_out
     for w in range(workers):
         if w == 0:
-            r_max_chunk = out_batch_size  + balance[w]
+            r_max_chunk = out_batch_size + balance[w]
         else:
             r_max_chunk = r_min_chunk + out_batch_size + balance[w]
         if r_max_chunk > max_out:
@@ -114,36 +107,31 @@ def main() -> None:
     import pandas as pd
     from pathlib import Path
 
-
     DOMAIN = (1, (2**8) - 1)
     RANGE = (1, (2**16) - 1)
-    # WORKERS = 8
-
-
+    WORKERS = 8
 
     # offset = (RANGE[1] + 1)// WORKERS
     # balance = [0] * WORKERS
     # balance = [-(offset) >> i for i in range(WORKERS)]
     # print(balance)
 
-    # for i in _batch_truth_scope(DOMAIN, RANGE, WORKERS):
-    #     print(i)
-    # count = 0
-    # operands = set()
-    # start = perf_counter()
-    # for i in _batch_truth_scope(DOMAIN, RANGE, WORKERS):
-    #     scope = list(mp.truth_scope(i[0], i[1]))
-    #     count += len(scope)
-    #     print(len(scope))
-    #     operands |= set(scope)
-    # end = perf_counter()
-    # print(f"Elapsed: {end - start}s")
-    # start = end
-    # print(count)
-    # print(len(operands))
-# -------------------------------------------------------------------
-
-
+    for i in _batch_truth_scope(DOMAIN, RANGE, WORKERS):
+        print(i)
+    count = 0
+    operands = set()
+    start = perf_counter()
+    for i in _batch_truth_scope(DOMAIN, RANGE, WORKERS):
+        scope = list(mp.truth_scope(i[0], i[1]))
+        count += len(scope)
+        print(len(scope))
+        operands |= set(scope)
+    end = perf_counter()
+    print(f"Elapsed: {end - start}s")
+    start = end
+    print(count)
+    print(len(operands))
+    # -------------------------------------------------------------------
 
     # for i in batch_truth_scope(DOMAIN, RANGE, WORKERS):
     #     print(i)
@@ -151,14 +139,13 @@ def main() -> None:
     # for i in batch_truth_scope(DOMAIN, RANGE, WORKERS):
     #     print(len(list(mp.truth_scope(i[0], i[1]))))
 
-
     # # pkl_alg = test_obj_pickling(alg)
     # # print(pkl_alg)
-    alg = mp.Algorithm(8)
-    alg.auto_resolve_stage()
-    for i in alg.exec(255, 255).values():
-        print(i)
-    print(alg)
+    # alg = mp.Algorithm(8)
+    # alg.auto_resolve_stage()
+    # for i in alg.exec(255, 255).values():
+    #     print(i)
+    # print(alg)
     # path = Path(__file__).parent.parent.parent / "examples/datasets/test_multi_new_map"
 
     # start = perf_counter()
@@ -179,7 +166,6 @@ def main() -> None:
     # df.to_parquet(path.parent / "test_df.parquet")
     # end = perf_counter()
     # print(f"\tTime: {end - start}")
-
 
     # import pandas as pd
     # import pyarrow as pa
