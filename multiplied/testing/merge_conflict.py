1576936c295f5099f8f1ed513b4a07122b20d4bd
EphraimCompEng <madugba.finance@proton.me>
2026-03-17 16:35:25 +0000

    testing!: mosaic-style templates expose bug #131

--- multiplied/testing/merge_conflict.py
+++ multiplied/testing/merge_conflict.py
@@ -5,8 +5,8 @@ from multiplied.tests import REFERENCE
 
 def main() -> None:
     alg = mp.Algorithm(8)
-    template = mp.Template(REFERENCE["complex_template"][8]["T"])
-    alg.push(template, mp.Map(REFERENCE["complex_map"][8]))
+    template = mp.Template(REFERENCE["mosaic_template"][8]["T"])
+    # alg.push(template, mp.Map(REFERENCE["complex_map"][8]))
     alg.auto_resolve_stage()
 
     for i in alg.exec(4, 20).values():
