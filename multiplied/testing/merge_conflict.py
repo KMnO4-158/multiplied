a49ffdca867c402f5f53a2e940d7f1f55a859fd3
EphraimCompEng <madugba.finance@proton.me>
2026-03-17 03:56:46 +0000

    testing: complex merge resolution

---
+++ multiplied/testing/merge_conflict.py
@@ -0,0 +1,18 @@
+import multiplied as mp
+from multiplied.tests import REFERENCE
+
+
+
+def main() -> None:
+    alg = mp.Algorithm(8)
+    template = mp.Template(REFERENCE["complex_template"][8]["T"])
+    alg.push(template, mp.Map(REFERENCE["complex_map"][8]))
+    alg.auto_resolve_stage()
+
+    for i in alg.exec(4, 20).values():
+        print(i)
+
+
+
+if __name__ == "__main__":
+    main()
